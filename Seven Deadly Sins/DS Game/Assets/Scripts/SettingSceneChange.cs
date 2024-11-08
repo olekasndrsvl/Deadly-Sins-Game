1c3def91086d89536c9b9e7b0c9aeced8b69e2f5
ir1nx <nikita058526@gmail.com>
2024-11-08 22:07:13 +0300

    Add delay before scene switch
    - Added a small delay before switching to another scene using a coroutine
    - Added sound for the exit button to the StartScene from DespondencyScene.

--- Seven Deadly Sins/DS Game/Assets/Scripts/SettingSceneChange.cs
+++ Seven Deadly Sins/DS Game/Assets/Scripts/SettingSceneChange.cs
@@ -8,6 +8,17 @@ public class SettingSceneChange : MonoBehaviour
     {
         SceneManager.LoadScene("StartScene", LoadSceneMode.Single);
     }
+
+    public void ChangeSceneWithDelay()
+    {
+        StartCoroutine(ChangeSceneCoroutine(1.5f));
+    }
+
+    private IEnumerator ChangeSceneCoroutine(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        SceneManager.LoadScene("StartScene", LoadSceneMode.Single);
+    }
     public void ChangeScene()
     {
         SceneManager.LoadScene("StartScene", LoadSceneMode.Single);
