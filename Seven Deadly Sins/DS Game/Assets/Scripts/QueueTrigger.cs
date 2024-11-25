c658a0099dd9f810493dbf054b7956039fc81962
Лужняк Александр <Lyzhniak2005@yandex.ru>
2024-11-25 20:14:34 +0300

    Added Skipping by dialogs

--- Seven Deadly Sins/DS Game/Assets/Scripts/QueueTrigger.cs
+++ Seven Deadly Sins/DS Game/Assets/Scripts/QueueTrigger.cs
@@ -1,17 +1,17 @@
-using UnityEditor.Build;
 using UnityEngine;
 
 public class QueueTrigger : MonoBehaviour
 {
+    bool IsinTrigger = false;
     public GameObject interactButton; // Ссылка на кнопку
     public GameObject dialogueBox;    // Ссылка на диалоговое окно
-    private Collider2D Collider;
+    public Collider2D Collider;
 
     void Start()
     {
         // Скрываем кнопку и диалог при старте
         interactButton.SetActive(false);
-        dialogueBox.SetActive(false);
+      
     }
 
     void OnTriggerEnter2D(Collider2D other)
@@ -22,19 +22,20 @@ public class QueueTrigger : MonoBehaviour
         {
             interactButton.SetActive(true); // Показываем кнопку
         }
+
+        IsinTrigger = true;
     }
 
     void OnTriggerExit2D(Collider2D other)
     {
+      //  Collider = null;
         // Если игрок выходит из триггера
         if (other.CompareTag("Player"))
         {
             interactButton.SetActive(false); // Скрываем кнопку
         }
+        IsinTrigger=false;
     }
 
-    public void OnDialogueButtonWithPlayer()
-    {
-
-    }
+   
 }
