a02ff8b963cf964cc6b58dbf2c2f08acb1d7147e
Лужняк Александр <Lyzhniak2005@yandex.ru>
2025-03-03 13:45:12 +0300

    Added main mehanics of BoastLevel

---
+++ Seven Deadly Sins/DS Game/Assets/Scripts/BoastSceneController.cs
@@ -0,0 +1,118 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BoastSceneController : MonoBehaviour
+{
+    public GameObject SceneLoadManager;
+    public GameObject LevelPreview;
+    public GameObject Mononlog;
+    public GameObject BoastTip;
+    public GameObject BoastTipText;
+    public int MonologResultWinValue = 2;
+    private int i = 0;
+    private bool MonologActivated = false;
+    private bool MonologEnded = false;
+    public List<string> Tips;
+    public List<string> WinTips;
+    public List<string> LoseTips;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        if (PlayerPrefs.GetInt("BoastPreviewIsDisplayed", 0) == 1)
+        {
+            LevelPreview.SetActive(false);
+            i= Tips.Count;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       if(!MonologActivated)
+       {
+           if (Input.touchCount > 0)
+           {
+               if (Input.GetTouch(0).phase != TouchPhase.Ended) return;
+               if (i < Tips.Count)
+               {
+                   StartCoroutine(SpellTip(Tips[i]));
+               }
+               else
+               {
+                   Mononlog.SetActive(true);
+                   BoastTip.SetActive(false);
+                   MonologActivated = true;
+               }
+
+
+           }
+       }
+       else
+       {
+           if (MonologEnded)
+           {
+               BoastTip.SetActive(true);
+               if (Mononlog.GetComponent<Monologue>().MonologResult == MonologResultWinValue)
+               {
+                   if (Input.touchCount > 0)
+                   {
+                       if (Input.GetTouch(0).phase != TouchPhase.Ended) return;
+                       if (i < WinTips.Count)
+                       {
+                           StartCoroutine(SpellTip(WinTips[i]));
+                       }
+                       else
+                       {
+                          SceneLoadManager.GetComponent<SceneChangeScript>().ChangeScene(2);
+                       }
+
+
+                   }
+               }
+               else
+               {
+                   if (Input.touchCount > 0)
+                   {
+                       if (Input.GetTouch(0).phase != TouchPhase.Ended) return;
+                       if (i < LoseTips.Count)
+                       {
+                           StartCoroutine(SpellTip(LoseTips[i]));
+                       }
+                       else
+                       {
+                           //Перезагрузка уровня
+                           SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+                           PlayerPrefs.SetInt("BoastPreviewIsDisplayed",1);
+                       }
+                   }
+               }
+               
+               
+           }
+       }
+    }
+
+    public void EndMonolog()
+    {
+        i = 0;
+        MonologEnded = true;
+    }
+    IEnumerator SpellTip(string s)
+    {
+        BoastTipText.GetComponent<TMP_Text>().text = "";
+        var sb = new StringBuilder();
+        foreach (var x in s + '\n')
+        {
+            sb.Append(x);
+            BoastTipText.GetComponent<TMP_Text>().text = sb.ToString();
+            yield return new WaitForSeconds(0.01f);
+        }
+
+        i++;
+    }
+}
